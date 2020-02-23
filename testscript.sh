696143d43fe4fd940bd06be709e842d97bcbfcb3
Nirmal Bhatt <nirmalgreen@gmail.com>
2020-02-23 17:05:49 +0000

    removed chandra

--- testscript.sh
+++ testscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "This is an example of script"
-echo "Nirmal chandra Bhatt"
+echo "Nirmal Bhatt"
 echo "This is running in a build step"
 echo "This is a param $PASSED"
 NUM=$(($FIRSTNUMBER + $SECONDNUMBER))
