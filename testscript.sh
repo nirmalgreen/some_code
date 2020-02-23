665eca1d2a40b7043138d35d00bffb5136f63df3
nirmalgreen <44209113+nirmalgreen@users.noreply.github.com>
2020-02-23 10:57:05 -0500

    Create testscript.sh

---
+++ testscript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "This is an example of script"
+echo "This is running in a build step"
+echo "This is a param $PASSED"
+NUM=$(($FIRSTNUMBER + $SECONDNUMBER))
+echo "$NUM"
+echo "This is complete"
